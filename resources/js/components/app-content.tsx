1baaa986d7609a8d03979ca69811458599a27dd7
John Cedric Panti <inhog.cedrick@gmail.com>
2025-03-17 10:08:05 +0800

    Add toaster component to root layout.

--- resources/js/components/app-content.tsx
+++ resources/js/components/app-content.tsx
@@ -1,11 +1,14 @@
 import { SidebarInset } from '@/components/ui/sidebar';
-import * as React from 'react';
+import { useAppearance } from '@/hooks/use-appearance';
+import { Toaster } from 'sonner';
 
 interface AppContentProps extends React.ComponentProps<'div'> {
   variant?: 'header' | 'sidebar';
 }
 
 export function AppContent({ variant = 'header', children, ...props }: AppContentProps) {
+  const { appearance } = useAppearance();
+
   if (variant === 'sidebar') {
     return <SidebarInset {...props}>{children}</SidebarInset>;
   }
@@ -13,6 +16,7 @@ export function AppContent({ variant = 'header', children, ...props }: AppConten
   return (
     <main className="mx-auto flex h-full w-full max-w-7xl flex-1 flex-col gap-4 rounded-xl p-4" {...props}>
       {children}
+      <Toaster theme={appearance} duration={10000} />
     </main>
   );
 }
