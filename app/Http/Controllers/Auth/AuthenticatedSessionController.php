16d72e5d53f810590f4f9df2d72d3124bbd6ff8d
John Cedric Panti <inhog.cedrick@gmail.com>
2025-03-28 11:14:18 +0800

    Update AuthenticatedSessionController for Role-Based Redirection
    * Updated AuthenticatedSessionController to redirect users based on their assigned role.
    * Implemented role-based logic to determine post-login landing pages.

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Enums\RoleEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
@@ -32,6 +33,10 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
+
+        if ($request->user()->hasRole(RoleEnum::REGULAR_USER->value)) {
+            return redirect()->intended(route('documents', absolute: false));
+        }
 
         return redirect()->intended(route('dashboard', absolute: false));
     }
