60cdf9d198a35850d10530eab6cab63abcc674ae
John Cedric Panti <inhog.cedrick@gmail.com>
2025-04-06 15:30:43 +0800

    Update api and documents routes
    * Added routes for approving and rejecting documents

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Api\UserController as ApiUserController;
 use App\Http\Controllers\Api\DocumentController as ApiDocumentController;
 
 Route::middleware(['auth', 'verified'])->prefix('api')->group(function () {
-  Route::get('users/search', [ApiUserController::class, 'search']);
-  Route::get('documents/{document}/download', [ApiDocumentController::class, 'download']);
+  Route::get('/users/search', [ApiUserController::class, 'search']);
+  Route::get('/documents/{document}/download', [ApiDocumentController::class, 'download']);
   Route::post('/validate-signatures', [SignatureController::class, 'validate']);
 });
--- routes/documents.php
+++ routes/documents.php
@@ -12,6 +12,12 @@ Route::post('/documents/create', [DocumentController::class, 'store'])->name('do
 
 Route::get('/documents/{document}', [DocumentController::class, 'show'])->name('documents.show');
 
+Route::post('/documents/{document}/approve', [DocumentController::class, 'approve'])->name('document.approve');
+
+Route::post('/documents/{document}/access', [DocumentController::class, 'updateAccess'])->name('documents.access.update');
+
+Route::post('/documents/{document}/reject', [DocumentController::class, 'reject'])->name('document.reject');
+
 Route::get('/documents/{document}/history', [DocumentController::class, 'history'])->name('documents.history');
 
 Route::patch('/documents/{document}/archive', [DocumentController::class, 'archive'])->name('documents.archive');
