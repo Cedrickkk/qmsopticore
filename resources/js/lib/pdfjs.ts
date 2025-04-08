6b7085f387c2300d9547d1735136c07a3707713d
John Cedric Panti <inhog.cedrick@gmail.com>
2025-04-08 19:13:08 +0800

    debug path

--- resources/js/lib/pdfjs.ts
+++ resources/js/lib/pdfjs.ts
@@ -5,3 +5,5 @@ import { pdfjs } from 'react-pdf';
  */
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL('pdfjs-dist/build/pdf.worker.min.mjs', import.meta.url).toString();
+
+console.log(pdfjs.GlobalWorkerOptions.workerSrc);
