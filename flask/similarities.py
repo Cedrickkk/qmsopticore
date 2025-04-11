7b19cce8b74cf907aed6107b7cc9e0c0266ff7b7
John Cedric Panti <inhog.cedrick@gmail.com>
2025-04-11 18:43:37 +0800

    Update similarities function

--- flask/similarities.py
+++ flask/similarities.py
@@ -1,15 +1,15 @@
 import numpy as np
 
-# def calculate_similarity(vector1, vector2):
-#     cosine_similarity = np.dot(vector1, vector2) / (
-#         np.linalg.norm(vector1) * np.linalg.norm(vector2)
-#     )
-# return float(np.clip(cosine_similarity, 0, 1))
-
 def calculate_similarity(vector1, vector2):
-    magnitude1 = np.linalg.norm(vector1)
-    magnitude2 = np.linalg.norm(vector2)
+    cosine_similarity = np.dot(vector1, vector2) / (
+        np.linalg.norm(vector1) * np.linalg.norm(vector2)
+    )
+    return cosine_similarity
 
-    cosine_similarity = np.dot(vector1, vector2) / (magnitude1 * magnitude2)
+# def calculate_similarity(vector1, vector2):
+#     magnitude1 = np.linalg.norm(vector1)
+#     magnitude2 = np.linalg.norm(vector2)
+
+#     cosine_similarity = np.dot(vector1, vector2) / (magnitude1 * magnitude2)
     
-    return float(np.clip(cosine_similarity, 0, 1))
+#     return float(np.clip(cosine_similarity, 0, 1))
