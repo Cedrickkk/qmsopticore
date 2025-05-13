1ca00543778202a2e7ff6c894ee1643c176e49d8
John Cedric Panti <inhog.cedrick@gmail.com>
2025-05-13 20:27:02 +0800

    Test initial automated deployment and updated the application version
    * Testing the initial automated deployment
    * Updated the application version with this feat

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -15,19 +15,21 @@ export default function AuthSplitLayout({ children, title, description }: AuthLa
       <div className="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
         <div className="bg-primary/20 absolute inset-0 bg-[url('/images/home-img.jpg')] bg-cover bg-right bg-no-repeat" />\
         <div className="bg-primary/50 absolute inset-0" />
-        <Link href={route('login')} className="relative z-20 flex items-center gap-4 text-lg font-medium">
-          <img src="/images/plp-1.png" alt="Pamantasan ng Lungsod ng Pasig Logo" className="size-12 scale-150" />
-          {name}
+        <Link href={route('login')} className="relative z-20 flex items-center gap-5 text-lg font-medium">
+          <img src="/images/plp-1.png" alt="Pamantasan ng Lungsod ng Pasig Logo" className="size-10 scale-150" />
+          <div className="flex flex-col items-start gap-1">
+            <span className="block text-sm uppercase">{name}</span>
+            <span className="block text-xs uppercase">Pamantasan ng Lungsod ng Pasig</span>
+          </div>
         </Link>
         {quote && (
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">&ldquo;{quote.message}&rdquo;</p>
-              <footer className="text-neutral text-sm">Hiraya Manawari</footer>
             </blockquote>
           </div>
         )}
-        <span className="text-neutral z-40 mt-4 self-end text-xs">QMS Version: {version}</span>
+        <span className="z-40 mt-4 text-xs font-medium uppercase">QMS Version: {version}</span>
       </div>
       <div className="w-full lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
