cfc6156d600d4f1963bc489c25e7cd39f0829806
John Cedric Panti <inhog.cedrick@gmail.com>
2025-07-05 09:33:35 +0800

    chore: update update email validation on user update

--- app/Http/Requests/Settings/ProfileUpdateRequest.php
+++ app/Http/Requests/Settings/ProfileUpdateRequest.php
@@ -26,6 +26,7 @@ class ProfileUpdateRequest extends FormRequest
                 'email',
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
+                'ends_with:@plpasig.edu.ph'
             ],
         ];
     }
