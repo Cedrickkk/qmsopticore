abeccb25ed75951cc45d45e3ca225f856b94db1f
John Cedric Panti <inhog.cedrick@gmail.com>
2025-08-05 11:06:23 +0800

    feat: extend DocumentRepositoryInterface for upcoming feature

--- app/Contracts/Repositories/DocumentRepositoryInterface.php
+++ app/Contracts/Repositories/DocumentRepositoryInterface.php
@@ -3,6 +3,7 @@
 namespace App\Contracts\Repositories;
 
 use App\Models\Document;
+use App\Models\User;
 
 interface DocumentRepositoryInterface
 {
@@ -10,8 +11,15 @@ interface DocumentRepositoryInterface
     public function create(array $attributes);
     public function update(Document $document, array $data);
     public function delete(Document $document);
-    public function paginate(?string $search = null);
+    public function paginate(?string $search, ?string $dateFrom = null, ?string $dateTo = null);
     public function paginateArchived(?string $search = null);
+    public function paginateAuthorizedDocuments(
+        User $user,
+        ?string $search = null,
+        ?string $dateFrom = null,
+        ?string $dateTo = null,
+        int $perPage = 10
+    );
     public function archive(Document $document);
     public function unarchive(Document $document);
     public function addSignatory(Document $document, array $data);
